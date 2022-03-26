dfb54f2bb9cb1ee2cadaba8536348472f17b83f5
Kaique Borges <rique.kaique@gmail.com>
2022-03-26 17:35:20 -0300

    Make EditingTaskTableRow's inputs writable

--- src/components/TaskTableRows/EditingTaskTableRow.tsx
+++ src/components/TaskTableRows/EditingTaskTableRow.tsx
@@ -1,25 +1,30 @@
+import { useState } from 'react'
 import { PrimaryButton, DangerButton } from '../Buttons'
 import { TextTableDataInput, NumberTableDataInput } from '../TableDataInputs/'
 import TaskTableRowProps from './TaskTableRowProps'
 
 const EditingTaskTableRow = ({ task }: TaskTableRowProps) => {
+  const [description, setDescription] = useState(task.description)
+  const [urgency, setUrgency] = useState(task.urgency)
+  const [importance, setImportance] = useState(task.importance)
+
   return (
     <tr>
       <td className="id">{task.id}</td>
       <TextTableDataInput
-        onChange={() => {}}
-        value={task.description}
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
         className="description"
       />
       <NumberTableDataInput
+        value={urgency}
+        onChange={(e) => setUrgency(parseInt(e.target.value))}
         className="urgency"
-        onChange={() => {}}
-        value={task.urgency}
       />
       <NumberTableDataInput
+        value={importance}
+        onChange={(e) => setImportance(parseInt(e.target.value))}
         className="importance"
-        onChange={() => {}}
-        value={task.importance}
       />
       <td className="actions">
         <PrimaryButton>Save</PrimaryButton>
